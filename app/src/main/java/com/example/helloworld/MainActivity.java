497451c5c672a367f7ed1f5d5d28cd2df13562f3
Vicky Kumar <imvickykumar999@gmail.com>
2021-04-12 18:07:20 +0530

    Completed

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onclick(View view) {
+
+        Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+
+    }
+}
