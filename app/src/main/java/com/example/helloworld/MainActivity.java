b7a641264592af4926202a739af0229e94acf301
imvickykumar999 <imvickykumar999@gmail.com>
2021-05-03 19:53:10 +0530

    app

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,22 +1,34 @@
-package com.example.helloworld;
+package com.example.helloworld.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onclick(View view) {
+        username = findViewById(R.id.et_username);
+        password = findViewById(R.id.et_password);
 
-        Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Welcome.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
